c6a3aade65bd87c3ae5cd8858de65d08b1a70e43
adamjonas <jonas@flatironschool.com>
2018-06-26 20:32:39 -0400

    First Commit

---
+++ test/index_test.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from ipynb.fs.full.index import *
+
+class TxTest(TestCase):
+
+    def test_sig_hash(self):
+        raw_tx = bytes.fromhex('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
+        stream = BytesIO(raw_tx)
+        tx = Tx.parse(stream)
+        hash_type = SIGHASH_ALL
+        want = int('27e0c5994dec7824e56dec6b2fcb342eb7cdb0d0957c2fce9882f715e85d81a6', 16)
+        self.assertEqual(tx.sig_hash(0, hash_type), want)
